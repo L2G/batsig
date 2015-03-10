b210059a204c5158462d4fbc505e407ef54d8bd4
Larry Gilbert <larry@L2G.to>
2015-03-10 12:41:27 -0700

    Crude beginnings

---
+++ listen.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+var Twit = require('twit')
+
+var listen = {
+  new_twit: function() {
+    return new Twit({
+      consumer_key:        process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
+      access_token:        process.env.TWITTER_ACCESS_TOKEN,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    })
+  }
+  ,
+  go: function() {
+    var t = this.new_twit()
+    var tw_user = 'adamcurry'
+    var tw_id
+
+    t.get('users/show', { 'screen_name': tw_user }, function(err, data, response) {
+      if (err) {
+        console.error('Twitter returned an error')
+        console.error(response)
+      }
+      else {
+        tw_id = data.id 
+        console.debug('Looked up Twitter screen name: ' + tw_user)
+        console.debug('Got ID: ' + tw_id)
+      }
+    })
+  }
+}
+listen.go()
