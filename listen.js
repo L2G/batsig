def9fda8f9b28e9e33652fc929397b7d1612836e
Larry Gilbert <larry@L2G.to>
2015-03-10 13:44:49 -0700

    JSLint the hell out of things

--- listen.js
+++ listen.js
@@ -1,34 +1,34 @@
-#!/usr/bin/env node
-var Twit = require('twit')
+'use strict';
 
-var log = require('./log.js')
+var Twit = require('twit');
+
+var log = require('./log.js');
 
 var listen = {
-  new_twit: function() {
-    return new Twit({
-      consumer_key:        process.env.TWITTER_CONSUMER_KEY,
-      consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
-      access_token:        process.env.TWITTER_ACCESS_TOKEN,
-      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-    });
-  }
-  ,
-  go: function() {
-    var t = this.new_twit();
-    var tw_user = 'adamcurry';
-    var tw_id;
+    new_twit: function () {
+        return new Twit({
+            consumer_key:        process.env.TWITTER_CONSUMER_KEY,
+            consumer_secret:     process.env.TWITTER_CONSUMER_SECRET,
+            access_token:        process.env.TWITTER_ACCESS_TOKEN,
+            access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+        });
+    },
 
-    t.get('users/show', { 'screen_name': tw_user }, function(err, data, response) {
-      if (err) {
-        log.error('Twitter returned an error');
-        log.error(response);
-      }
-      else {
-        tw_id = data.id;
-        log.debug('Looked up Twitter screen name: ' + tw_user);
-        log.debug('Got ID: ' + tw_id);
-      };
-    });
-  }
-}
-listen.go()
+    go: function () {
+        var t = this.new_twit(),
+            tw_user = 'adamcurry',
+            tw_id;
+
+        t.get('users/show', { 'screen_name': tw_user }, function (err, data, response) {
+            if (err) {
+                log.error('Twitter returned an error');
+                log.error(response);
+            } else {
+                tw_id = data.id;
+                log.debug('Looked up Twitter screen name: ' + tw_user);
+                log.debug('Got ID: ' + tw_id);
+            }
+        });
+    }
+};
+listen.go();
--- log.js
+++ log.js
@@ -1,17 +1,16 @@
+'use strict';
+
 exports.info = function (msg) {
-  return console.log('I ' + msg);
+    return console.log('I ' + msg);
 };
 
 exports.debug = function (msg) {
-  if (process.env.DEBUG) {
-    return console.log('D ' + msg);
-  }
-  else {
-    return
-  };
-}
+    if (process.env.DEBUG) {
+        return console.log('D ' + msg);
+    }
+};
 
 exports.error = function (msg) {
-  return console.error('E ' + msg);
+    return console.error('E ' + msg);
 };
 
