6760e317b02bbf8489699a5a1a0dc51ac8210c19
Larry Gilbert <larry@l2g.to>
2015-03-12 22:53:18 -0700

    Add in EventEmitter to TunerBase

--- tuner_base.js
+++ tuner_base.js
@@ -2,10 +2,14 @@
 
 var stampit = require('stampit');
 
+var EventEmitter = stampit.convertConstructor(
+    require('events').EventEmitter
+);
+
 var TunerBase = stampit({
     tuneIn: function() {
         throw 'tuneIn not yet implemented';
     }
 });
 
-module.exports = TunerBase;
+module.exports = stampit.compose(EventEmitter, TunerBase);
