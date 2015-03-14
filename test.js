1fbf4c7cc159cfe5027b647922879de97a06a163
Larry Gilbert <larry@l2g.to>
2015-03-14 15:20:32 -0700

    More rearranging of TunerTwitter

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var TunerTwitter = require('./tuner_twitter');
 
 var tt = TunerTwitter.create({twitterName: 'L2G'});
 log.debug('created new TunerTwitter');
-tt.tuneIn();
 tt.on('ready', function () {
     log.info('TunerTwitter says it is ready');
 })
+tt.tuneIn();
--- tuner_twitter.js
+++ tuner_twitter.js
@@ -8,33 +8,43 @@ var twitterCreds = require('./twitter_creds')();
 
 var TunerTwitter = TunerBase.compose(
     stampit({
-        tuneIn: function() {
+        tuneIn: function tuneIn() {
             if (this.twitterID) {
                 log.debug('Already have Twitter ID: ' + this.twitterID);
-                return;
+                this.tunerReady();
+            } else {
+                this.lookUpTwitterIdFor(this.twitterName);
             }
-
-            var twitClient = new Twit(twitterCreds);
-            var self = this;
-
-            twitClient.get('users/show',
-                           { 'screen_name': self.twitterName },
-                           function (err, data, response) {
-                if (err) {
-                    return log.error('Twitter returned an error ' + response);
-                }
-
-                log.debug('Looked up Twitter screen name: ' + self.twitterName);
-                log.debug('Got ID: ' + data.id);
-                self.twitterID = data.id;
-                if (self.emit('ready')) { return; }
-                return log.error('No one was listening to me! (TunerTwitter)');
-            });
+        },
+        tunerReady: function tunerReady() {
+            if (!this.twitterID) {
+                log.error("TunerTwitter.tunerReady() called, but there's still no twitterID!");
+            } else if (!this.emit('ready')) {
+                log.error('No one was listening to TwitterReady. Sad face!');
+            }
         }
     },
     {
         twitterName: null,
         twitterID:   null
+    },
+    function() {
+        var outerObject = this,
+            twitClient = new Twit(twitterCreds);
+
+        this.lookUpTwitterIdFor = function lookUpTwitterIdFor(name) {
+            twitClient.get('users/show',
+                           { 'screen_name': name },
+                           function (err, data, response) {
+                               if (err) {
+                                   return log.error('Twitter returned an error ' + response);
+                               }
+                               log.debug('Looked up Twitter screen name: ' + name);
+                               log.debug('Got ID: ' + data.id);
+                               outerObject.twitterID = data.id;
+                               outerObject.tunerReady();
+                           });
+        };
     })
 );
 
