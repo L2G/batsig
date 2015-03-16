ca59a15ec14922d44e5893983dae88fcdfefe697
Larry Gilbert <larry@l2g.to>
2015-03-15 20:20:42 -0700

    RepeaterNull methods are generic enough to move into RepeaterBase

--- repeater_base.js
+++ repeater_base.js
@@ -1,11 +1,47 @@
 'use strict';
 
 var stampit = require('stampit');
+var log = require('./log');
 
 var RepeaterBase = stampit({
-    addTuner: function() {
-        throw 'addTuner not yet implemented';
-    }
+    readyHandler: function readyHandler(tuner) {
+        var repeater = this;
+        return function () {
+            log.info('Tuner reports it is ready; ' +
+                     repeater.name + ' is now tuning it in');
+            tuner.tuneIn();
+        };
+    },
+    tunedInHandler: function tunedInHandler(tuner) {
+        return function () {
+            log.info('Tuner is tuned in');
+        };
+    },
+    messageHandler: function messageHandler(tuner) {
+        var repeater = this;
+        return function (message) {
+            log.info(repeater.name + ' received message: ' + message);
+        };
+    },
+    errorHandler: function errorHandler(tuner) {
+        return function (message) {
+            log.error('Tuner raised an error: ' + message);
+        };
+    },
+    lostHandler: function lostHandler(tuner) {
+        return function (message) {
+            log.error('Tuner lost its connection: ' + message);
+        };
+    },
+    addTuner: function addTuner(tuner) {
+        log.info(this.name + ' is adding new tuner');
+        tuner.on('ready',   this.readyHandler(tuner));
+        tuner.on('tunedIn', this.tunedInHandler(tuner));
+        tuner.on('message', this.messageHandler(tuner));
+        tuner.on('error',   this.errorHandler(tuner));
+        tuner.on('lost',    this.lostHandler(tuner));
+        tuner.setup();
+    },
 },
 {
     name: 'Nameless repeater'
--- repeater_null.js
+++ repeater_null.js
@@ -5,47 +5,7 @@ var stampit = require('stampit');
 var RepeaterBase = require('./repeater_base');
 var log = require('./log');
 
-var RepeaterNull = stampit({
-    readyHandler: function readyHandler(tuner) {
-        var repeater = this;
-        return function () {
-            log.info('Tuner reports it is ready; ' +
-                     repeater.name + ' is now tuning it in');
-            tuner.tuneIn();
-        };
-    },
-    tunedInHandler: function tunedInHandler(tuner) {
-        return function () {
-            log.info('Tuner is tuned in');
-        };
-    },
-    messageHandler: function messageHandler(tuner) {
-        var repeater = this;
-        return function (message) {
-            log.info(repeater.name + ' received message: ' + message);
-        };
-    },
-    errorHandler: function errorHandler(tuner) {
-        return function (message) {
-            log.error('Tuner raised an error: ' + message);
-        };
-    },
-    lostHandler: function lostHandler(tuner) {
-        return function (message) {
-            log.error('Tuner lost its connection: ' + message);
-        };
-    },
-    addTuner: function addTuner(tuner) {
-        log.info(this.name + ' is adding new tuner');
-        tuner.on('ready',   this.readyHandler(tuner));
-        tuner.on('tunedIn', this.tunedInHandler(tuner));
-        tuner.on('message', this.messageHandler(tuner));
-        tuner.on('error',   this.errorHandler(tuner));
-        tuner.on('lost',    this.lostHandler(tuner));
-        tuner.setup();
-    },
-},
-{
+var RepeaterNull = stampit().state({
     name: 'Null repeater'
 });
 
