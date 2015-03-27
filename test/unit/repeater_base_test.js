f1f8b107f4e8ce0231b27b4571308ba0ed7fcfeb
Larry Gilbert <larry@l2g.to>
2015-03-26 22:54:54 -0700

    Lay some groundwork for repeaterBase.addTuner() tests

--- test/unit/repeater_base_test.js
+++ test/unit/repeater_base_test.js
@@ -1,4 +1,6 @@
 var expect = require('expect.js');
+var sinon = require('sinon');
+
 var repeaterBase = require('../../repeater_base');
 
 describe('repeaterBase', function () {
@@ -40,9 +42,30 @@ describe('repeaterBase', function () {
         });
     });
 
-    describe('#addTuner', function () {
+    describe('#addTuner()', function () {
+        var signalHandlers = {
+            'ready':   'readyHandler',
+            'tunedIn': 'tunedInHandler',
+            'message': 'messageHandler',
+            'lost':    'lostHandler',
+            'error':   'errorHandler'
+        };
+        var stubs = [];
+
+        before(function () {
+            Object.keys(signalHandlers).map(function (signal) {
+                stubs.push(sinon.stub(instance, signalHandlers[signal]));
+            });
+        });
+
         it('should be defined', function () {
             expect(instance.addTuner).to.be.a(Function);
+        });
+
+        after(function () {
+            stubs.forEach(function (stub) {
+                stub.restore();
+            });
         });
     });
 
