35925140b21174af22b22111fae89e7040cfcac4
Larry Gilbert <larry@l2g.to>
2015-03-31 12:43:17 -0700

    Add tests for registration of handlers in repeaterBase
    This is broken at the moment because the tuner setup() method is still
    being used, but we don't intend to have the repeaters call this anymore.

--- test/unit/repeater_base_test.js
+++ test/unit/repeater_base_test.js
@@ -5,6 +5,7 @@ var expect = require('expect.js');
 var sinon = require('sinon');
 
 var repeaterBase = require('../../repeater_base');
+var tunerBase = require('../../tuner_base');
 
 describe('repeaterBase', function () {
     var instance = repeaterBase.create();
@@ -54,15 +55,26 @@ describe('repeaterBase', function () {
             'error':   'errorHandler'
         };
         var stubs = [];
+        var tuner = tunerBase.create();
+        var mock;
 
         before(function () {
             Object.keys(signalHandlers).map(function (signal) {
                 stubs.push(sinon.stub(instance, signalHandlers[signal]));
             });
+            mock = sinon.mock(tuner);
         });
 
         it('should be defined', function () {
             expect(instance.addTuner).to.be.a(Function);
+        });
+
+        it('should register all expected handlers', function () {
+            Object.keys(signalHandlers).forEach(function (signal) {
+                mock.expects('on').withArgs(signal).once();
+            });
+            instance.addTuner(tuner);
+            mock.verify();
         });
 
         after(function () {
